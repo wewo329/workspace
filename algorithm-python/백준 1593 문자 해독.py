b89d96a77e24fd3cfd195df50bcaf9016f7ded3a
Tineil <gurdnrnjs19@gmail.com>
2021-08-29 02:44:51 +0900

    plus commit

--- algorithm-python/백준 1593 문자 해독.py
+++
@@ -1,9 +0,0 @@
-import sys
-w_len, s_len = map(int, sys.stdin.readline().split())
-w = "".join(sorted(list(input())))
-s = input()
-result = []
-for i in range(s_len-(w_len-1)):
-    if "".join(sorted(list(s[i:i+w_len]))) == w:
-        result.append(s[i:i+w_len])
-print(len(set(result)))
